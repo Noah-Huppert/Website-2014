fad83f21849d9500ebd6e6df44413e4d382b66e8
Noah Huppert <noahhuppert@gmail.com>
2014-09-03 10:50:42 -0400

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 clear
 echo "Set new root password"
-echo "Add new user `noah`, only fill out `Full Name` as `Noah Huppert`"
+echo "Add new user 'noah', only fill out 'Full Name' as 'Noah Huppert'"
 read ok
 sudo passwd
 sudo adduser noah
 echo "Install nano"
 sudo apt-get install nano
-echo "Change user `noah`s permissions with ssh"
-echo "add `noah ALL=(ALL:ALL) ALL` below `root ALL...`"
+echo "Change user 'noah's permissions with ssh"
+echo "add 'noah ALL=(ALL:ALL) ALL' below 'root ALL...'"
 read ok
 sudo visudo
 echo "Modify ssh setup"
